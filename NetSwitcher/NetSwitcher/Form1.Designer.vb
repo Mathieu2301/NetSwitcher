ff86f6be090bba98943e6f9a244c52790bafb504
Mathieu Colmon <mathieu@usp-3.fr>
2018-01-26 21:21:20 +0100

    Initial commit

---
+++ NetSwitcher/NetSwitcher/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ADSLToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NetSwitcher"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ADSLToolStripMenuItem, Me.GToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(103, 48)
+        '
+        'ADSLToolStripMenuItem
+        '
+        Me.ADSLToolStripMenuItem.Name = "ADSLToolStripMenuItem"
+        Me.ADSLToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.ADSLToolStripMenuItem.Text = "ADSL"
+        '
+        'GToolStripMenuItem
+        '
+        Me.GToolStripMenuItem.Name = "GToolStripMenuItem"
+        Me.GToolStripMenuItem.Size = New System.Drawing.Size(102, 22)
+        Me.GToolStripMenuItem.Text = "4G"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(223, 132)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "NetSwitcher"
+        Me.TransparencyKey = System.Drawing.Color.Red
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ADSLToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ NetSwitcher/NetSwitcher/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim status = True
+
+    Sub disable() '4G
+        send("int set int name=""Ethernet 5"" admin=disabled")
+        status = False
+        NotifyIcon1.BalloonTipTitle = "NetSwitcher"
+        NotifyIcon1.BalloonTipText = "Vous venez de passer sur la 4G"
+        NotifyIcon1.ShowBalloonTip(5000)
+        GToolStripMenuItem.Enabled = False
+        ADSLToolStripMenuItem.Enabled = True
+    End Sub
+
+    Sub enable() 'ADSL
+        send("int set int name=""Ethernet 5"" admin=enabled")
+        status = True
+        NotifyIcon1.BalloonTipTitle = "NetSwitcher"
+        NotifyIcon1.BalloonTipText = "Vous venez de passer sur l'ADSL"
+        NotifyIcon1.ShowBalloonTip(5000)
+        ADSLToolStripMenuItem.Enabled = False
+        GToolStripMenuItem.Enabled = True
+    End Sub
+
+    Sub send(ByVal command)
+        Dim psi As New ProcessStartInfo() ' Initialize ProcessStartInfo (psi)
+        psi.Verb = "runas" ' runas = Run As Administrator
+        psi.FileName = "netsh" ' File or exe to run (this cannot take arguments, use ProcessStartInfo.Arguments instead
+        psi.Arguments = command ' Arguments for the process that you're going to run
+        Try
+            Process.Start(psi) ' Run the process (User is required to press Yes to run the program with Administrator access)
+        Catch
+            MsgBox("User cancelled the operation", 16, "") ' User pressed No
+        End Try
+    End Sub
+
+    Private Sub ADSLToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ADSLToolStripMenuItem.Click
+        enable()
+    End Sub
+
+    Private Sub GToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GToolStripMenuItem.Click
+        disable()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        If status Then
+            disable()
+        Else
+            enable()
+        End If
+    End Sub
+End Class
